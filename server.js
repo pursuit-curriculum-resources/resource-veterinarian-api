488772a9c4fdd2c256134cd235411db62f3b0e5c
Wes Reid <bwreid@gmail.com>
2021-10-26 07:53:10 -0700

    Update default port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "data", "db.json"));
 const middleware = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Enable all CORS requests.
 app.use(cors());
